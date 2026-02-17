ec2df67fc336f578710755bcf82e650fef6bebcb
hezar <hezarmoh@gmail.com>
2026-02-17 17:40:15 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter To-Do App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TodoScreen(),
+    );
+  }
+}
+
+class TodoScreen extends StatefulWidget {
+  const TodoScreen({super.key});
+
+  @override
+  _TodoScreenState createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  List<Map<String, dynamic>> tasks = [];
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My To-Do List"), centerTitle: true),
+      body: Column(
+        children: [
+          // INPUT FIELD
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hintText: "Enter new task",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: addTask, child: Text("ADD")),
+              ],
+            ),
+          ),
+
+          // TASK LIST
+          Expanded(
+            child: tasks.isEmpty
+                ? Center(
+                    child: Text("No tasks yet", style: TextStyle(fontSize: 18)),
+                  )
+                : ListView.builder(
+                    itemCount: tasks.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 5,
+                        ),
+                        child: ListTile(
+                          // TASK TITLE
+                          title: Text(
+                            tasks[index]['title'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              decoration: tasks[index]['completed']
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                            ),
+                          ),
+
+                          // CHECKBOX
+                          leading: Checkbox(
+                            value: tasks[index]['completed'],
+                            onChanged: (value) {
+                              toggleTask(index, value);
+                            },
+                          ),
+
+                          // DELETE BUTTON
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              deleteTask(index);
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
